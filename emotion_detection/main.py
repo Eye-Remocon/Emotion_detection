de34845025da22985b14351feba6b4fef3dcb448
hithere1012 <hithere1012@naver.com>
2021-08-01 00:17:55 +0900

    Changes to host, ip and None error

--- emotion_detection/main.py
+++ emotion_detection/main.py
@@ -1,13 +1,10 @@
 import base64
-import cv2
 import io
-
 import numpy
 import aws_emotion_detection
 import home_emotion_detection
 from PIL import Image
 from flask import Flask, request, jsonify
-
 
 
 app = Flask(__name__)
@@ -21,17 +18,13 @@ def main():
     face_img = Image.open(buf).convert('RGB')
     open_cv_face_image = numpy.array(face_img)
 
-
-
     detection_result = home_emotion_detection.detection(open_cv_face_image)
-    if detection_result == '':
+    if detection_result == None:
         detection_result = aws_emotion_detection.aws_main(im_binary)
 
     return jsonify({"emotion":detection_result})
 
-
-
 if __name__ == "__main__":
-    app.run(debug=True, host='192.168.0.4', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=9900)
 
 
